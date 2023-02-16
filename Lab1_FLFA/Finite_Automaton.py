be0ce3f17b2c42f5b92c253c8cee1d7383fa8c29
ninico11 <112957880+ninico11@users.noreply.github.com>
2023-02-16 08:02:01 +0200

    Finite Automaton Class

--- Lab1_FLFA/Finite_Automaton.py
+++ Lab1_FLFA/Finite_Automaton.py
@@ -7,19 +7,24 @@ class Finite_Automaton:
         self.q0 = q0
         self.F = F
     # Method which check if word is valid
-    def string_belongs_to_language(self,input_string):
+    def string_belongs_to_language(self, input_string):
         # Set the current state to the initial state
         current_state = self.q0
-
+        nr = 0
         # For each symbol in the input string, find the next state using the delta function
         for symbol in input_string:
-            if (current_state, symbol) in self.delta:
-                current_state = self.delta[(current_state, symbol)]
-            else:
-                continue
-
-        # If the current state is in the set of final states, return True
-        if current_state in self.F:
-            return True
-        else:
-            return False
+            if ((current_state[0], symbol) in self.delta) and (nr != len(input_string)-1):
+                current_state = self.delta[(current_state[0], symbol)]
+            elif nr == len(input_string) - 1:
+                if len(current_state) > 1:
+                    # If the current state is in the set of final states, return True
+                    if current_state[len(current_state)-1] in self.F:
+                        return True
+                    else:
+                        return False
+                else:
+                    if current_state[0] in self.F:
+                        return True
+                    else:
+                        return False
+            nr += 1
