85d364c5691e6716acc77937bf386840157351fd
ninico11 <112957880+ninico11@users.noreply.github.com>
2023-02-16 08:03:39 +0200

    Main

--- Lab1_FLFA/main.py
+++ Lab1_FLFA/main.py
@@ -1,28 +1,26 @@
 import Grammar
-# Set of non-terminal symbols
-V_N = {'S', 'B', 'C', 'D'}
-# Set of terminal symbols
+
+V_N = {'S', 'B', 'C', 'D', 'X'}
 V_T = {'a', 'b', 'c'}
-# Set of productions, rules or transitions
 P = {
     "S": ["aB"],
-    "B": ["bS", "aC", "b"],
+    "B": ["bS", "aC", "bX"],
     "C": ["bD"],
-    "D": ["a", "bC", "cS"],
-
+    "D": ["aX", "bC", "cS"],
+    "X": " "
 }
-# Initiate the grammar
+
 grammar = Grammar.Grammar(V_N, V_T, P)
-# Initiate hte finite automaton
+
 finite_automaton = grammar.to_finite_automaton()
-# Create five words
+
 s1 = grammar.generate_string()
 s2 = grammar.generate_string()
 s3 = grammar.generate_string()
 s4 = grammar.generate_string()
 s5 = grammar.generate_string()
-print(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 )
-# Verify if words are valid
+print(s1)
+
 print("s1 is " + str(finite_automaton.string_belongs_to_language(s1)))
 print("s2 is " + str(finite_automaton.string_belongs_to_language(s2)))
 print("s3 is " + str(finite_automaton.string_belongs_to_language(s3)))
